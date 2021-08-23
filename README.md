7d73c1bbaa61f7524ca7105ae31c339cb8ab6b10
Matt Czapliński <deindesign.pl@gmail.com>
2021-08-24 01:13:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# NativeScript Vue Router
+
+[nativescript-vue-router](https://github.com/mattCCC/nativescript-vue-router)
+
+NativeScript Vue Router brings API compatiblility from web version of Vue Router. Please file an issue or make a PR if you spot any problems or you have any further requests regarding the functionality.
+
+## Prerequisites / Requirements
+
+Nativescript 7.1+ is required for the plugin to run properly. It might be working on previous NS6 although the plugin is no longer officially supported for NativeScript 6.
+
+## Installation
+
+```javascript
+tns plugin add nativescript-vue-router
+
+or
+
+npm install nativescript-vue-router
+
+or
+
+yarn add nativescript-vue-router
+```
+
+## Usage
+
+[nativescript-vue-router](https://www.npmjs.com/package/nativescript-vue-router)
+
+Currently under development.
+
+## License
+
+Apache License Version 2.0, January 2004
---
+++ src/.eslintrc.js
@@ -0,0 +1,85 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        createDefaultProgram: true,
+        "project": "src/tsconfig.json",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "@typescript-eslint/tslint"
+    ],
+    "rules": {
+        "@typescript-eslint/indent": "off",
+        "@typescript-eslint/member-delimiter-style": [
+            "error",
+            {
+                "multiline": {
+                    "delimiter": "semi",
+                    "requireLast": true
+                },
+                "singleline": {
+                    "delimiter": "semi",
+                    "requireLast": false
+                }
+            }
+        ],
+        "@typescript-eslint/naming-convention": "error",
+        "no-underscore-dangle": "off",
+        "@typescript-eslint/prefer-namespace-keyword": "error",
+        "@typescript-eslint/quotes": [
+            "off",
+            "double"
+        ],
+        "@typescript-eslint/semi": [
+            "error",
+            "always"
+        ],
+        "@typescript-eslint/type-annotation-spacing": "error",
+        "brace-style": [
+            "error",
+            "1tbs"
+        ],
+        "eqeqeq": [
+            "error",
+            "smart"
+        ],
+        "id-blacklist": [
+            "error",
+            "any",
+            "Number",
+            "number",
+            "String",
+            "string",
+            "Boolean",
+            "boolean",
+            "Undefined",
+            "undefined"
+        ],
+        "id-match": "error",
+        "no-eval": "error",
+        "no-redeclare": "error",
+        "no-trailing-spaces": "error",
+        "no-var": "error",
+        "spaced-comment": [
+            "error",
+            "always",
+            {
+                "markers": [
+                    "/"
+                ]
+            }
+        ],
+        "@typescript-eslint/tslint/config": [
+            "error",
+            {
+                "rules": {
+                    "whitespace": true
+                }
+            }
+        ]
+    }
+};
